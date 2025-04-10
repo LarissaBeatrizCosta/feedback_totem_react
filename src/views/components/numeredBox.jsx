8f24cb02910460a3d625f597fae809b1074492da
larissa beatriz costa <larissa@grupovex.com.br>
2025-04-10 11:39:39 -0300

    style: :lipstick: modify shape and color of rating buttons

--- src/views/components/numeredBox.jsx
+++ src/views/components/numeredBox.jsx
@@ -1,30 +1,38 @@
 import { Box, Button, Typography } from "@mui/material";
 
+/**
+ * NumeredBox renders a button with a numerical value and a background color based
+ * on the rating (low, medium, high).
+ *
+ * @param {{ number: number, isSelected: boolean, onClick: function }} props
+ *
+ * @returns {React.ReactElement} The component to be rendered.
+ */
 function NumeredBox({ number, isSelected, onClick }) {
-  let colorMap = {
-    1: "red",
-    2: "#f4d03f",
-    3: "green",
+  const colorMap = {
+    low: "red",
+    medium: "#f4d03f",
+    high: "green",
   };
 
-  let color = number < 6 ? 1 : number < 8 ? 2 : 3;
+  const color = number < 6 ? "low" : number < 8 ? "medium" : "high";
 
   return (
-    <Button onClick={onClick} sx={{ padding: "0px" }}>
+    <Button onClick={onClick} sx={{ padding: 0 }}>
       <Box
         sx={{
           backgroundColor: colorMap[color],
-          border: isSelected ? "5px solid black" : "none",
-          width: "70px",
-          height: "70px",
+          border: isSelected ? 4 : 0,
+          width: 70,
+          height: 70,
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          borderRadius: "10px",
+          borderRadius: 10,
         }}
       >
         <Typography
-          sx={{ color: "white", fontWeight: "bold", fontSize: "25px" }}
+          sx={{ color: "white", fontWeight: "bold", fontSize: 25 }}
         >
           {number}
         </Typography>
--- src/views/components/numeredList.jsx
+++ src/views/components/numeredList.jsx
@@ -1,20 +1,28 @@
 import NumeredBox from "../components/numeredBox";
 import useRatingStore from "../../store/useRatingStore";
 
+/**
+ * NumeredBoxList renders a row of boxes with numbers from 1 to 10. The component
+ * uses the `useRatingStore` hook to get the current rating and the
+ * `setSelectedRating` function to update the rating when a box is clicked.
+ *
+ * @returns {JSX.Element} A row of boxes with numbers from 1 to 10.
+ */
 function NumeredBoxList() {
   const { rating, setRating } = useRatingStore();
 
-  const handleClick = (number) => {
-    setRating(number);
+  const handleClick = (selectedRating) => {
+    setRating(selectedRating);
   };
 
   return (
     <>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((number) => (
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
         <NumeredBox
-          number={number}
-          isSelected={rating === number}
-          onClick={() => handleClick(number)}
+          key={num}
+          number={num}
+          isSelected={rating === num}
+          onClick={() => handleClick(num)}
         />
       ))}
     </>
